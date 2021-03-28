80024911f461b4caaea6288ff9d78a76a4c829a7
chandra <chandra@anterin.id>
2021-03-28 15:59:22 +0700

    fix ssl

--- config/index.js
+++ config/index.js
@@ -38,13 +38,14 @@ module.exports = {
   
     production: {
         client: process.env.DB_CONNECTION,
-        ssl: true,
         connection: {
           host :  process.env.DB_HOST,
           port : process.env.DB_PORT,
           database: process.env.DB_DATABASE,
           user:     process.env.DB_USERNAME,
           password: process.env.DB_PASSWORD,
+          ssl: { rejectUnauthorized: false }
+
         },
         pool: {
           min: 2,
